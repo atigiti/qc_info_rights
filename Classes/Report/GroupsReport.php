55ddf9701c65ce4135524b40021317f492896f7a
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2022-05-05 13:41:23 +0000

    first commit

--- Classes/Report/GroupsReport.php
+++ Classes/Report/GroupsReport.php
@@ -2,7 +2,95 @@
 
 namespace Qc\QcInfoRights\Report;
 
-class GroupsReport
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Http\JsonResponse;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\View\StandaloneView;
+
+class GroupsReport extends QcInfoRightsReport
 {
+    /**
+     * Initializes the Module
+     */
+    protected function initialize()
+    {
+        parent::initialize();
+        $pageRenderer = $this->moduleTemplate->getPageRenderer();
+        $pageRenderer->loadRequireJsModule('TYPO3/CMS/QcInfoRights/ShowMembers');
+    }
+    /**
+     * Create tabs to split the report and the checkLink functions
+     */
+    protected function renderContent(): string
+    {
+        if (!$this->isAccessibleForCurrentUser) {
+            // If no access or if ID == zero
+            $this->moduleTemplate->addFlashMessage(
+                $this->getLanguageService()->getLL('no.access'),
+                $this->getLanguageService()->getLL('no.access.title'),
+                FlashMessage::ERROR
+            );
+            return '';
+        }
+
+        $menuItems = [];
+        if ($this->showTabGroups) {
+            $menuItems[] = [
+                'label' => $this->getLanguageService()->getLL('beUserGroupsLists'),
+                'content' => $this->createViewForBeUserGroupListTab()->render()
+            ];
+        }
+
+
+        return $this->moduleTemplate->getDynamicTabMenu($menuItems, 'report-qcinforights');
+    }
+
+    /**
+     * Displays the View for the Backend User List
+     *
+     * @return StandaloneView
+     */
+    protected function createViewForBeUserGroupListTab(): StandaloneView
+    {
+        $this->filter = $this->backendSession->get('qc_info_rights_key');
+        if (GeneralUtility::_GP('groupPaginationPage') != null ){
+            $groupPaginationCurrentPage = (int)GeneralUtility::_GP('groupPaginationPage');
+            // Store the current page on session
+            $this->filter = $this->backendSession->get('qc_info_rights_key');
+            $this->filter->setCurrentGroupsTabPage($groupPaginationCurrentPage);
+            $this->updateFilter();
+        }
+        else{
+            // read from Session
+            $groupPaginationCurrentPage = $this->filter->getCurrentGroupsTabPage();
+        }
+        $view = $this->createView('BeUserGroupList');
+        $pagination = $this->getPagination($this->backendUserGroupRepository->findAll(), $groupPaginationCurrentPage,$this->groupsPerPage );
+
+        $view->assignMultiple([
+            'prefix' => 'beUserGroupList',
+            'backendUserGroups' => $pagination['paginatedData'],
+            'showExportGroups' => $this->showExportGroups,
+            'showMembersColumn' => $this->checkShowTsConfig('showMembersColumn'),
+            'pagination' => $pagination['pagination'],
+            'currentPage' => $this->id,
+            'args' => $this->set
+        ]);
+        return $view;
+    }
+
+    // show members
+    /**
+     * This Function is delete the selected excluded link
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function showMembers(ServerRequestInterface $request): ResponseInterface{
+        $urlParam = $request->getQueryParams();
+        $members = $this->backendUserRepository->getGroupMembers($urlParam['groupUid'], $urlParam['selectedColumn']);
+        return new JsonResponse($members);
+    }
 
 }
--- ext_tables.php
+++ ext_tables.php
@@ -11,9 +11,26 @@ call_user_func(static function() {
 // Extend Module INFO with new Element
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction(
     'web_info',
-    QcInfoRightsReport::class,
+    \Qc\QcInfoRights\Report\AccessRightsReport::class,
     '',
     'LLL:EXT:qc_info_rights/Resources/Private/Language/locallang.xlf:mod_qcInfoRight'
+);
+
+// Extend Module INFO with new Element
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction(
+    'web_info',
+    \Qc\QcInfoRights\Report\GroupsReport::class,
+    '',
+    'groupes',
+
+);
+
+// Extend Module INFO with new Element
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction(
+    'web_info',
+    \Qc\QcInfoRights\Report\UsersReport::class,
+    '',
+    'utilisateurs'
 );
 
 // Initialize Context Sensitive Help (CSH)
